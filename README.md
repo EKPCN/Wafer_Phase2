dc7871599ed21274e97dd6891ff60fb894d446d7
Daniel Schell <ugctg@student.kit.edu>
2014-10-31 11:49:19 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Wafer_Phase2
+============
+
+Ruby layout script for sensor R&D for CMS Upgrade Phase 2
+For documentation see: http://schellda.github.io/Wafer_Phase2/
--- utils/merge.rb
+++ utils/merge.rb
@@ -1,6 +1,11 @@
 module Merge
   include RBA
 
+  # Merge two polygons that touch eachother
+  # @param poly1 [Polygon] Polygon 1
+  # @param poly2 [Polygon] Polygon 2
+  # @return [Polygon] Returns merged polygon
+
   def Merge.polyPair(poly1,poly2)
   
     edgeProc = EdgeProcessor.new
@@ -9,6 +14,9 @@ module Merge
     return endPoly[0]
   end
 
+  # Merge a array of polygons where neigboring polygon entries touch eachother
+  # @param polyVec [Array<Polygon>] Array of polygons
+  # @return [Polygon] Returns the merged polygon
 
   def Merge.polyVector(polyVec)
   
@@ -22,6 +30,12 @@ module Merge
     return poly[polyVec.size-1]
   end
 
+  # Insert one cell into another
+  # @param cell1 [Cell] cell that contains cell2
+  # @param cell2 [Cell] cell that will be placed into cell1
+  # @param x0 [Int] x coordinate where the lower left corner of cell2 will be placed in cell1
+  # @param y0 [Int] y coordinate where the lower left corner of cell2 will be placed in cell1
+  # @return [Nill]
 
   def Merge.cells(cell1,cell2,x0=0,y0=0)
     cell1.insert(CellInstArray::new(cell2.cell_index,CplxTrans::new(1,0,false,DPoint::new(x0,y0))))
