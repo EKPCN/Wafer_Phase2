ff694b81a0f5cb60f8e558d8a757ce59aec4d1c8
Daniel Schell <daniel.schell@kit.edu>
2015-12-07 21:55:05 +0100

    Updated design for ATLAS CiS run

--- sensors/r4s50x50PT/r4s50x50PT.rb
+++ sensors/r4s50x50PT/r4s50x50PT.rb
@@ -17,7 +17,7 @@ module R4S50x50PT
     Pixel.ptBiasLine($layerAlu,InnerPixel['cellSizeX'],InnerPixel['cellSizeY'],InnerPixel['PTX0'],InnerPixel['PTY0'],InnerPixel['bDotDia'],InnerPixel['bLWidth'])
     Pixel.ptPStop($layerPp,InnerPixel['PTX0'],InnerPixel['PTY0'],(InnerPixel['bDotDia']+InnerPixel['PTholeDia']-2.0*InnerPixel['metalOH'])/2.0-InnerPixel['PTpStopWidth'],(InnerPixel['bDotDia']+InnerPixel['PTholeDia']-2.0*InnerPixel['metalOH'])/2.0+InnerPixel['PTpStopWidth'])
     Pixel.bumpPad($layerAlu,InnerPixel['bPDia'],InnerPixel['bPX0'],InnerPixel['bPY0'])
-    Pixel.bumpPad($layerPassivation,InnerPixel['bPDiaPassivation'],InnerPixel['bPX0'],InnerPixel['bPY0'])
+    Pixel.bumpPad($layerPassOpen,InnerPixel['bPDiaPassivation'],InnerPixel['bPX0'],InnerPixel['bPY0'])
     Pixel.ptVia($layerAluVia,InnerPixel['viaX0'],InnerPixel['viaY0'],InnerPixel['viaDia'])
     Pixel.pStop($layerPp, InnerPixel['implantSizeX']+2*InnerPixel['PSdistX'], InnerPixel['implantSizeY']+2*InnerPixel['PSdistY'], InnerPixel['PSwidth'], InnerPixel['PSrOut'] , InnerPixel['PSrIn'], InnerPixel['PSopenX0'], InnerPixel['PSopenY0'], InnerPixel['PSopenWidth'],true)
     
@@ -29,7 +29,7 @@ module R4S50x50PT
     # Last Pixel Row
     
     Pixel.grid(innerPixelCell,1, InnerPixel['nY'], InnerPixel['dX'], InnerPixel['dY'], PixelGrid['sizeX']/2-InnerPixel['cellSizeX']/2, -PixelGrid['sizeY']/2+InnerPixel['cellSizeY']/2)
-    Pixel.grid(innerPixelCell,1, InnerPixel['nY'], InnerPixel['dX'], InnerPixel['dY'], PixelGrid['sizeX']/2-InnerPixel['cellSizeX']/2, -PixelGrid['sizeY']/2+3*InnerPixel['cellSizeY']/2,0,true)
+    #Pixel.grid(innerPixelCell,1, InnerPixel['nY'], InnerPixel['dX'], InnerPixel['dY'], PixelGrid['sizeX']/2-InnerPixel['cellSizeX']/2, -PixelGrid['sizeY']/2+3*InnerPixel['cellSizeY']/2,0,true)
       
     
     Merge.cells($sensor, pixelGridCell)
