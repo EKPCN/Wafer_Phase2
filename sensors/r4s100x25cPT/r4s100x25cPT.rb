221a5d35d5e82e4573a57a1780c3161f647b7351
Daniel Schell <daniel.schell@kit.edu>
2016-04-17 00:57:00 +0200

    Added new periphery to serveral other r4s100x25 sensors

--- sensors/r4s100x25cPT/r4s100x25cPT.rb
+++ sensors/r4s100x25cPT/r4s100x25cPT.rb
@@ -7,6 +7,8 @@ module R4S100x25cPT
 
   def R4S100x25cPT.create(layout,sensor)
     
+    rocType = "R4S"
+    
     $sensor = layout.create_cell(sensor)
     name = sensor + "."
     
@@ -58,7 +60,7 @@ module R4S100x25cPT
 
     periCell = layout.create_cell(name+"Periphery")
     Periphery.init(periCell)
-    Periphery.create($layerNp,$layerAlu,$layerPassOpen,$layerPpe19,$layerAluVia,PixelGrid,BiasRing,GuardRing,PixelEdge)
+    Periphery.create($layerNp,$layerAlu,$layerPassOpen,$layerPpe19,$layerAluVia,PixelGrid,BiasRing,GuardRing,PixelEdge,rocType)
 
     #bump pads for guard ring connection
 #    Pixel.bumpPad($layerPassOpen,InnerPixel['bPDiaPassivation'],-InnerPixel['bPX0']-PixelGrid['sizeX']/2,InnerPixel['bPY0']-PixelGrid['sizeY']/2+(InnerPixel['cellSizeY']/2)-225e3)
